efa6ec8f4bf9fe79b76ad6bd566ce19b995aec47
Dev-JeongDaeRi <kor216465@gmail.com>
2023-04-17 21:08:10 +0900

    12일차 zip

---
+++ app/src/main/java/com/jeongdaeri/kotlin_tip_of_the_day/basic/12_zip.kt
@@ -0,0 +1,12 @@
+package com.jeongdaeri.kotlin_tip_of_the_day.basic
+
+fun main(){
+
+    // 콜렉션
+    val myFriends = listOf("철수", "제임스", "영희", "제시카")
+
+    val numbers = listOf(1, 4, 7, 9, 10, 20)
+
+    val result = myFriends.zip(numbers)
+
+}
